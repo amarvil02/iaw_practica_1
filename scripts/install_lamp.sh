377b7a24750e59dc9a416227335eee1230d39fde
amarvil02 <adrianmartinezvilchez02@gmail.com>
2023-10-06 12:35:35 +0000

    Se actualiza install_lamp.sh, se crean 2 archivos.

--- scripts/install_lamp.sh
+++ scripts/install_lamp.sh
@@ -4,7 +4,7 @@
 set -x
 
 # Actualizamos los repositorios
-#apt update
+apt update
 
 # Actualizamos los paquetes
 #apt upgrade -y
@@ -14,3 +14,18 @@ apt install apache2 -y
 
 # Instalamos el sistema gestor de base de datos MySQL
 apt install mysql-server -y
+
+# Instalamos PHP
+sudo apt install php libapache2-mod-php php-mysql -y
+
+# Copiar el archivo de configuración de Apache
+cp ../conf/000-default.conf /etc/apache2/sites-available
+
+# Reiniciamos el servicio de Apache
+systemctl restart apache2
+
+# Copiamos el archivo de prueba de PHP
+cp ../php/index.php /var/www/html
+
+# Modificamos el propietario y el grupo del directorio /var/www/html
+chown -R www-data:www-data /var/www/html
