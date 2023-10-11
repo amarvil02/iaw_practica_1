ff49f98c5592cb8fa49a3a6a6e52c713a0493627
amarvil02 <adrianmartinezvilchez02@gmail.com>
2023-10-11 12:30:13 +0000

    Configura la autenticación básica de directorio

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -3,7 +3,7 @@
 # Muestra todos los comandos que se van ejecutando
 set -x
 
-#Se importan las variables de configuración
+#Paso 1. Se importan las variables de configuración
 source .env
 
 # Actualizamos los repositorios
@@ -12,7 +12,7 @@ apt update
 # Actualizamos los paquetes
 #apt upgrade -y
 
-# Configuramos las respuestas de instalación de phpMyAdmin
+# Pase 2.Configuramos las respuestas de instalación de phpMyAdmin
 echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/mysql/app-pass password $PHPMYADMIN_APP_PASSWORD" | debconf-set-selections
@@ -26,7 +26,7 @@ mysql -u root <<< "DROP USER IF EXISTS '$APP_USER'@'%'"
 mysql -u root <<< "CREATE USER '$APP_USER'@'%' IDENTIFIED BY '$APP_PASSWORD';"
 mysql -u root <<< "GRANT ALL PRIVILEGES ON *.* TO '$APP_USER'@'%'";
 
-# Instalamos Adminer
+# Paso 3. Instalamos Adminer
 # Creamos el directorio para adminer
 mkdir -p /var/www/html/adminer
 
@@ -39,7 +39,7 @@ mv /var/www/html/adminer/adminer-4.8.1-mysql.php /var/www/html/adminer/index.php
 # Modificamos el propietario y el grupo del directorio /var/www/html
 chown -R www-data:www-data /var/www/html
 
-# Instalamos GoAccess
+# Paso 4. Instalamos GoAccess
 apt install goaccess -y
 
 # Creamos un directorio para los informes html de GoAccess
@@ -47,3 +47,14 @@ mkdir -p /var/www/html/stats
 
 # Ejecutamos GoAccess en segundo plano
 goaccess /var/log/apache2/access.log -o /var/www/html/stats/index.html --log-format=COMBINED --real-time-html --daemonize
+
+
+# Paso 5. Configuramos la autenticacion basica de un directorio
+# Creamos el archivo .htpasswd
+htpasswd -bc /etc/apache2/.htpasswd $STATS_USERNAME $STATS_PASSWORD
+
+# Copiamos el archivo de configuración de Apache con la configuración del acceso al directorio
+cp ../conf/000-default-stats.conf /etc/apache2/sites-available/000-default.conf
+
+# Reiniciamos el servicio de Apache
+systemctl restart apache2
